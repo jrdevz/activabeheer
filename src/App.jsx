5563ebdb50152f16fd3205ca922253f98b3dd743
Jonathan Rier <jonathan.rier@icloud.com>
2025-01-10 14:46:57 +0100

    Main table component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import {InventoryTable} from './components/Table/Table'
 
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <InventoryTable/>
     </div>
   )
 }
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,7 +1,33 @@
-export default function Table() {
+import './Table.css'
+
+export function InventoryTable() {
     return (
         <table>
-            
+            <thead>
+                <menu>
+                    <li><input type="text" placeholder='Search' /></li>
+                    <li></li>
+                    <li></li>
+                </menu>
+                <tr>
+                    <th><input type="checkbox" /></th>
+                    <th>Serienummer</th>
+                    <th>Merk</th>
+                    <th>Model</th>
+                    <th>Aankoopdatum</th>
+                    <th>Garantiedatum</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><input type="checkbox" /></td>
+                    <td>LT-084525401753</td>
+                    <td>Microsoft</td>
+                    <td>Surface Pro 7</td>
+                    <td>16-07-2023</td>
+                    <td>16-07-2025</td>
+                </tr>
+            </tbody>
         </table>
     );
 }
